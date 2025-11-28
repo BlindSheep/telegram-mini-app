d8efb13fbab54bab172c4dfe8bfa04b1a8d2f511
aleksejkuvaev <aleksey-kuvaev@mail.ru>
2025-11-28 11:42:16 +0300

    Translated readme

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,6 @@ use MoonShine\TlgMiniApp\Http\Middleware\TelegramAuth;
 use Illuminate\Support\Facades\Route;
 use MoonShine\Contracts\Core\DependencyInjection\RouterContract;
 
-
-
-
 Route::moonshine(static function () {
     Route::view('/telegram-startapp', 'ms-telegram-mini-app::startapp')
         ->name('telegram-startapp');
